218bd301c82ee5f6cf53f4667c87934edd6b232a
Alberto Vecina Sánchez <albertovecina@gmail.com>
2020-09-17 14:48:21 +0200

    *[AdsDetail] Added page indicator for the pictures

--- list/src/main/java/com/idealista/android/challenge/list/ui/ads/AdsActivity.kt
+++ list/src/main/java/com/idealista/android/challenge/list/ui/ads/AdsActivity.kt
@@ -3,6 +3,7 @@ package com.idealista.android.challenge.list.ui.ads
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.idealista.android.challenge.core.Addressable
 import com.idealista.android.challenge.list.R
 import kotlinx.android.synthetic.main.activity_ads.*
@@ -15,9 +16,33 @@ class AdsActivity : AppCompatActivity(),
         setContentView(R.layout.activity_ads)
         AdsAssembler.presenter =
             AdsPresenter(this)
+
+        initViews()
+
         AdsAssembler.presenter.onAdNeeded(
             intent.getStringExtra(Addressable.Activity.Ads.EXTRA_URL) ?: ""
         )
+    }
+
+    private fun initViews() {
+        vpPictures.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                updatePageIndicator(position + 1)
+            }
+
+        })
     }
 
     override fun showProgress() {
@@ -34,10 +59,19 @@ class AdsActivity : AppCompatActivity(),
                 PicturePageAdapter(
                     model.pictures
                 )
+            updatePageIndicator(1)
             tvTitle.text = title
             tvPrice.text = price
             tvComment.text = comment
         }
+    }
+
+    private fun updatePageIndicator(page: Int) {
+        tvPageIndicator.text = getString(
+            R.string.page_indicator,
+            page,
+            vpPictures?.adapter?.count ?: 0
+        )
     }
 
 }
